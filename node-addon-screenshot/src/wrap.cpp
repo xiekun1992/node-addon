1f6e0b32e8deeaf624fa3d1757d51acfafafa404
xiekun1992 <xkfront@outlook.com>
2019-05-28 00:51:36 +0800

    修改图像

--- node-addon-screenshot/src/wrap.cpp
+++ node-addon-screenshot/src/wrap.cpp
@@ -3,7 +3,7 @@
 Napi::Value wrap::getBitmap(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
     BYTE* bytes = screenshot::getBitmap();
-    Napi::Buffer<BYTE> buf = Napi::Buffer<BYTE>::New(env, bytes, 1600 * 900);
+    Napi::Buffer<BYTE> buf = Napi::Buffer<BYTE>::New(env, bytes, 1600 * 900 * 4);
     // Napi::Value buf = info[0].As<Napi::Number>();
     // free(bytes);
     return buf;
